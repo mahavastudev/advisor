56a123de9dc981affda8b082b8d1f01701a690b3
Ojasvi Wadhwa <developer@mahavastu.com>
2021-08-10 14:10:01 +0530

    Remove comment

--- backend/advisor/src/main/java/com/mahavastu/advisor/controller/SiteController.java
+++ backend/advisor/src/main/java/com/mahavastu/advisor/controller/SiteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-// @CrossOrigin("http://localhost:4200")
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("site")
 public class SiteController {
 
