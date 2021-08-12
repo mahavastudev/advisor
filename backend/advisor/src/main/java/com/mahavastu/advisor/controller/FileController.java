2938f011eee916f67953167c32ae3e0138caa0e5
Ojasvi Wadhwa <developer@mahavastu.com>
2021-08-13 01:38:38 +0530

    Upload File change request type

--- backend/advisor/src/main/java/com/mahavastu/advisor/controller/FileController.java
+++ backend/advisor/src/main/java/com/mahavastu/advisor/controller/FileController.java
@@ -1,8 +1,8 @@
 package com.mahavastu.advisor.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class FileController
     private FileService fileService;
     
     @PostMapping
-    public String uploadFile(@RequestBody TransportFile transportFile)
+    public String uploadFile(@ModelAttribute TransportFile transportFile)
     {
         return fileService.uploadFile(transportFile);
     }
